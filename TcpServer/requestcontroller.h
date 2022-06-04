860b7b377727c04514111d5943bc84d0d16d01dd
Parallel23333 <58279449+373050201@users.noreply.github.com>
2022-06-04 12:48:01 +0800

    Add files via upload

--- TcpServer/requestcontroller.h
+++ TcpServer/requestcontroller.h
@@ -3,8 +3,8 @@
 
 class RequestController {
 public:
-    void startRequest(int mode,int capacity);       //创建请求
-    void changeRequest(int mode,int value);     //更改请求
-    void endRequest();      //结束请求
-    void newQueueNum(int mode);     //分配新的排队号
+    char* startRequest(int mode,int capacity);       //创建请求
+    char* changeRequest(int mode,int value);     //更改请求
+    char* endRequest();      //结束请求
+    char* newQueueNum(int mode);     //分配新的排队号
 };
