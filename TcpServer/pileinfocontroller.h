577e44b328c7562753382d25b364241a767febbe
hkRuan <100742311+hkRuan@users.noreply.github.com>
2022-06-10 15:49:02 +0800

    Update pileinfocontroller.h

--- TcpServer/pileinfocontroller.h
+++ TcpServer/pileinfocontroller.h
@@ -8,7 +8,7 @@
 class PileInfoController
 {
 public:
-    QEventLoop loop;
+    //QEventLoop loop;
     std::string generatePileInfo(std::string pileNo);  //生成某充电桩状态信息，供服务器调用
     std::string generateCarInfo(std::string pileNo);  //生成某充电桩排队车辆信息，供服务器调用
     std::string getReport(std::string pileNo);        //生成报表，供服务器调用
