cee0c66dff3abd258731397f022bb68132ed9e5e
hkRuan <100742311+hkRuan@users.noreply.github.com>
2022-06-10 15:42:25 +0800

    Update pileinfocontroller.cpp

--- TcpServer/pileinfocontroller.cpp
+++ TcpServer/pileinfocontroller.cpp
@@ -1,67 +1,221 @@
 #include "pileinfocontroller.h"
 #include "global.h"
-//目前loop还没用明白，就先注释掉了
+#include <stdlib.h>
 std::string PileInfoController::generatePileInfo(std::string pileNo)    //生成某充电桩状态信息，供服务器调用
 {
     //根据pileNo获取充电桩的socket，给它发请求消息select
-    qintptr descriptor=Global::mstr2Int.at(pileNo);
+    qintptr descriptor;
     QString msg="select/0\t";
-    for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+    QString ans="yes/";
+    if(pileNo=="0")
     {
-        QTcpSocket *item = Global::tcpclientsocketlist.at(i);
-        if(item->socketDescriptor() == descriptor)
+        for(int i=1; i<=Global::fastChargingPileNum; i++)
         {
-            item->write(msg.toUtf8().data());
-            item->flush();
-            qDebug()<<"send to pile: "<<descriptor<<msg;
-            break;
+            descriptor=Global::mstr2Int.at(("F"+QString::number(i)).toStdString());
+            for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+            {
+                QTcpSocket *item = Global::tcpclientsocketlist.at(i);
+                if(item->socketDescriptor() == descriptor)
+                {
+                    item->write(msg.toUtf8().data());
+                    item->flush();
+                    qDebug()<<"send to pile: "<<descriptor<<msg;
+                    break;
+                }
+            }
+            //Global::loopNo=2;
+            //loop.exec();
+            Global::mutex.lock();
+            Global::condition.wait(&Global::mutex);
+            //给充电桩发完请求后，阻塞等待充电桩回复
+            //回复消息收到，存在buffer中
+            ans += Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+            Global::mutex.unlock();
+        }
+        for(int i=1; i<=Global::trickleChargingPileNum; i++)
+        {
+            descriptor=Global::mstr2Int.at(("T"+QString::number(i)).toStdString());
+            for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+            {
+                QTcpSocket *item = Global::tcpclientsocketlist.at(i);
+                if(item->socketDescriptor() == descriptor)
+                {
+                    item->write(msg.toUtf8().data());
+                    item->flush();
+                    qDebug()<<"send to pile: "<<descriptor<<msg;
+                    break;
+                }
+            }
+            //Global::loopNo=2;
+            //loop.exec();
+            Global::mutex.lock();
+            Global::condition.wait(&Global::mutex);
+            //给充电桩发完请求后，阻塞等待充电桩回复
+            //回复消息收到，存在buffer中
+            ans += Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+            Global::mutex.unlock();
         }
     }
-    //给充电桩发完请求后，阻塞等待充电桩回复
-    //回复消息收到，存在buffer中
-    //Global::loop.exec();
-    QString ans = Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+    else
+    {
+        descriptor=Global::mstr2Int.at(pileNo);
+        for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+        {
+            QTcpSocket *item = Global::tcpclientsocketlist.at(i);
+            if(item->socketDescriptor() == descriptor)
+            {
+                item->write(msg.toUtf8().data());
+                item->flush();
+                qDebug()<<"send to pile: "<<descriptor<<msg;
+                break;
+            }
+        }
+        //Global::loopNo=2;
+        //loop.exec();
+        Global::mutex.lock();
+        Global::condition.wait(&Global::mutex);
+        //给充电桩发完请求后，阻塞等待充电桩回复
+        //回复消息收到，存在buffer中
+        ans += Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+        Global::mutex.unlock();
+    }
+    qDebug()<<ans;
     return ans.toStdString();
 }
 std::string PileInfoController::generateCarInfo(std::string pileNo) //生成某充电桩排队车辆信息，供服务器调用
 {
-    qintptr descriptor=Global::mstr2Int.at(pileNo);
+    qintptr descriptor;
     QString msg="select/1\t";
-    for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+    QString ans="yes/";
+    if(pileNo=="0")
     {
-        QTcpSocket *item = Global::tcpclientsocketlist.at(i);
-        if(item->socketDescriptor() == descriptor)
+        for(int i=1; i<=Global::fastChargingPileNum; i++)
         {
-            item->write(msg.toUtf8().data());
-            item->flush();
-            qDebug()<<"send to pile: "<<descriptor<<msg;
-            break;
+            descriptor=Global::mstr2Int.at(("F"+QString::number(i)).toStdString());
+            for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+            {
+                QTcpSocket *item = Global::tcpclientsocketlist.at(i);
+                if(item->socketDescriptor() == descriptor)
+                {
+                    item->write(msg.toUtf8().data());
+                    item->flush();
+                    qDebug()<<"send to pile: "<<descriptor<<msg;
+                    break;
+                }
+            }
+            //Global::loopNo=2;
+            //loop.exec();
+            Global::mutex.lock();
+            Global::condition.wait(&Global::mutex);
+            //给充电桩发完请求后，阻塞等待充电桩回复
+            //回复消息收到，存在buffer中
+            ans += Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+            Global::mutex.unlock();
+        }
+        for(int i=1; i<=Global::trickleChargingPileNum; i++)
+        {
+            descriptor=Global::mstr2Int.at(("T"+QString::number(i)).toStdString());
+            for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+            {
+                QTcpSocket *item = Global::tcpclientsocketlist.at(i);
+                if(item->socketDescriptor() == descriptor)
+                {
+                    item->write(msg.toUtf8().data());
+                    item->flush();
+                    qDebug()<<"send to pile: "<<descriptor<<msg;
+                    break;
+                }
+            }
+            //Global::loopNo=2;
+            //loop.exec();
+            Global::mutex.lock();
+            Global::condition.wait(&Global::mutex);
+            //给充电桩发完请求后，阻塞等待充电桩回复
+            //回复消息收到，存在buffer中
+            ans += Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+            Global::mutex.unlock();
         }
     }
-    //给充电桩发完请求后，阻塞等待充电桩回复
-    //回复消息收到，存在buffer中
-    //Global::loop.exec();
-    QString ans = Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+    else
+    {
+        descriptor=Global::mstr2Int.at(pileNo);
+        for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+        {
+            QTcpSocket *item = Global::tcpclientsocketlist.at(i);
+            if(item->socketDescriptor() == descriptor)
+            {
+                item->write(msg.toUtf8().data());
+                item->flush();
+                qDebug()<<"send to pile: "<<descriptor<<msg;
+                break;
+            }
+        }
+       // Global::loopNo=2;
+        //loop.exec();
+        Global::mutex.lock();
+        Global::condition.wait(&Global::mutex);
+        //给充电桩发完请求后，阻塞等待充电桩回复
+        //回复消息收到，存在buffer中
+        ans += Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+        Global::mutex.unlock();
+    }
+    qDebug()<<ans;
     return ans.toStdString();
 }
 std::string PileInfoController::getReport(std::string pileNo)//生成报表，供服务器调用
 {
-    qintptr descriptor=Global::mstr2Int.at(pileNo);
+    qintptr descriptor;
     QString msg="select/2\t";
-    for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+    QString ans="yes/";
+    if(pileNo=="0")
     {
-        QTcpSocket *item = Global::tcpclientsocketlist.at(i);
-        if(item->socketDescriptor() == descriptor)
+        for(int i=1; i<=Global::fastChargingPileNum; i++)
         {
-            item->write(msg.toUtf8().data());
-            item->flush();
-            qDebug()<<"send to pile: "<<descriptor<<msg;
-            break;
+            descriptor=Global::mstr2Int.at(("F"+QString::number(i)).toStdString());
+            for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+            {
+                QTcpSocket *item = Global::tcpclientsocketlist.at(i);
+                if(item->socketDescriptor() == descriptor)
+                {
+                    item->write(msg.toUtf8().data());
+                    item->flush();
+                    qDebug()<<"send to pile: "<<descriptor<<msg;
+                    break;
+                }
+            }
+           // Global::loopNo=2;
+            //loop.exec();
+            Global::mutex.lock();
+            Global::condition.wait(&Global::mutex);
+            //给充电桩发完请求后，阻塞等待充电桩回复
+            //回复消息收到，存在buffer中
+            ans += Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+            Global::mutex.unlock();
+        }
+        for(int i=1; i<=Global::trickleChargingPileNum; i++)
+        {
+            descriptor=Global::mstr2Int.at(("T"+QString::number(i)).toStdString());
+            for(int i = 0; i < Global::tcpclientsocketlist.count(); i++)
+            {
+                QTcpSocket *item = Global::tcpclientsocketlist.at(i);
+                if(item->socketDescriptor() == descriptor)
+                {
+                    item->write(msg.toUtf8().data());
+                    item->flush();
+                    qDebug()<<"send to pile: "<<descriptor<<msg;
+                    break;
+                }
+            }
+           // Global::loopNo=2;
+            //loop.exec();
+            Global::mutex.lock();
+            Global::condition.wait(&Global::mutex);
+            //给充电桩发完请求后，阻塞等待充电桩回复
+            //回复消息收到，存在buffer中
+            ans += Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+            Global::mutex.unlock();
         }
     }
-    //给充电桩发完请求后，阻塞等待充电桩回复
-    //回复消息收到，存在buffer中
-    //Global::loop.exec();
-    QString ans = Global::buffer[1]; //返回给服务器的消息，让服务器转发给管理员
+    else qDebug()<<"no/error\t";
     return ans.toStdString();
 }
